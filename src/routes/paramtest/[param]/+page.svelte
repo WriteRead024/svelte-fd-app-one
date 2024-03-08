84a84c2a59465a90c7d0376241a571a2b692c18d
WriteRead024 <rich@rwhifonline.net>
2024-03-08 11:22:55 -0500

    more parameter testing text

--- src/routes/paramtest/[param]/+page.svelte
+++ src/routes/paramtest/[param]/+page.svelte
@@ -10,4 +10,5 @@
 <h1>Parameter Testing</h1>
 <p>created Feb. 14th 2024</p>
 <p>the route parameter value is '{data.paramvar}'</p>
+<p>it does update nicely when the address URI is changed, not sure about the page load cycle though</p>
 <p><a href="/">back to the main home page</a></p>
