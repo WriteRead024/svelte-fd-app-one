c0931c0acf398f1a1a7bcb86f0ef04190744a4d4
WriteRead024 <rich@rwhifonline.net>
2024-03-08 12:13:06 -0500

    more text prep for file rename

--- src/routes/page-two/+page.svelte
+++ src/routes/page-two/+page.svelte
@@ -4,7 +4,7 @@
 
 <h3>Welcome to SvelteKit</h3>
 <h2>svelte-fd-app-one</h2>
-<h1>Page Two</h1>
+<h1>New Test Page Two</h1>
 <p>created Feb., Mar. 2024</p>
 <p>
     This page doesn't do anything but it does have a fun fact about this app:
