6f915dc0b434d7e4851c94e2627410cb38f47211
WriteRead024 <rich@rwhifonline.net>
2024-03-08 12:12:33 -0500

    page two fun fact

--- src/routes/page-two/+page.svelte
+++ src/routes/page-two/+page.svelte
@@ -5,5 +5,10 @@
 <h3>Welcome to SvelteKit</h3>
 <h2>svelte-fd-app-one</h2>
 <h1>Page Two</h1>
-<p>created Feb. 14th 2024</p>
+<p>created Feb., Mar. 2024</p>
+<p>
+    This page doesn't do anything but it does have a fun fact about this app:
+    hyperlinks in this 'svelte-fd-app-one' project that go to other external websites
+    are double-underlined.&nbsp; Links to pages within this website are single-underlined.
+</p>
 <p><a href="/">back to the main home page</a></p>
