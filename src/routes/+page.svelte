ebaa849bbf8d33d5592a7a17c12c52ed08ff7f68
WriteRead024 <rich@rwhifonline.net>
2024-03-08 11:42:59 -0500

    double-underline external links by css class 'ext'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h3>Welcome to SvelteKit</h3>
 <h1>svelte-fd-app-one</h1>
 for "svelte - first development (test) app one"
-<p>see <a href="https://kit.svelte.dev">kit.svelte.dev</a> for documentation</p>
+<p>see <a class="ext" href="https://kit.svelte.dev">kit.svelte.dev</a> for documentation</p>
 <p>project started Feb. 14th 2024</p>
 <p><a href="/page-two">Page Two</a></p>
 <p><a href="/keypad">Keypad Page</a></p>
@@ -10,4 +10,4 @@ for "svelte - first development (test) app one"
 <p><a href="/learning-notes/one">Learning Notes</a></p>
 <br>
 <br>
-<p><a href="https://svelte.dev/examples/hello-world">In-browser example sandbox</a></p>
+<p><a class="ext" href="https://svelte.dev/examples/hello-world">In-browser example sandbox</a></p>
--- src/routes/learning-notes/[param]/One.svelte
+++ src/routes/learning-notes/[param]/One.svelte
@@ -1,23 +1,23 @@
 <p>
-    <a href="https://kit.svelte.dev/docs/introduction">introduction</a>:
+    <a class="ext" href="https://kit.svelte.dev/docs/introduction">introduction</a>:
     SvelteKit is a development kit and code base built around the Svelte UI component renderer.&nbsp;
     It includes a URL router and a bunch of optimizations and rendering options.&nbsp;
     Svelte/SvelteKit can instantly update a test website to reflect code changes, which seems to be a main point to Svelte.&nbsp;
 </p>
 <p>
-    <a href="https://kit.svelte.dev/docs/creating-a-project">creating a project</a>:
+    <a class="ext" href="https://kit.svelte.dev/docs/creating-a-project">creating a project</a>:
     'npm create svelte@latest new-app-name' is featureful.&nbsp; There is good tooling support.
 </p>
 <p>
-     <a href="https://kit.svelte.dev/docs/project-structure">project structure</a>:
+     <a class="ext" href="https://kit.svelte.dev/docs/project-structure">project structure</a>:
     there are special files for "lib" and "server lib" scripts, routing "parameter matchers", 
     and specially named files for "hooks" and "service workers".&nbsp; There is a folder for static files.
     There are several folders for different types of tests, there is significant test tool support built in.
     There are several configuration files, including a "svelte.config.js" file
-    <a href="https://kit.svelte.dev/docs/configuration">(documentation)</a>.
+    <a class="ext" href="https://kit.svelte.dev/docs/configuration">(documentation)</a>.
 </p>
 <p>
-    <a href="https://kit.svelte.dev/docs/web-standards">web standards</a>:
+    <a class="ext" href="https://kit.svelte.dev/docs/web-standards">web standards</a>:
     SvelteKit uses the 
     fetch API, FormData objects, Stream APIs, URL APIs, URLSearchParams for query parameters, Web Crypto.
 </p>
